c209e1974c900cd2270e34bc30a9668da0081a10
barbabjetolov <rizz.edoardo@gmail.com>
2021-03-18 21:22:37 +0100

    reformatting + logging

--- http-service/main.go
+++ http-service/main.go
@@ -1,63 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
-	"strings"
+	"os"
 
 	"github.com/barbabjetolov/endocode-test/http-service/pkg/utilities"
-	"github.com/fatih/camelcase"
-	logrus "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
-// compile time variables
-var (
-	GitCommit   string
-	ProjectName string
-)
+func init() {
 
-// handler for the /helloworld endpoint
-func HandlerHelloworld(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		q := r.URL.Query()
-		if name, isIn := q["name"]; isIn {
-
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(`Hello ` + strings.Join(camelcase.Split(name[0]), " ")))
-		} else {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(`Hello Stranger`))
-		}
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(`Method not found!`))
-	}
-}
-
-// handler for the /versionz endpoint
-func HandlerVersionz(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		resStruct := &utilities.ResponseVersionz{GitCommit: GitCommit,
-			ProjectName: ProjectName}
-
-		resBytes, err := json.Marshal(resStruct)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(`Internal server error!`))
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(resBytes))
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(`Method not found!`))
-	}
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
 }
 
 func main() {
-	http.HandleFunc("/helloworld", HandlerHelloworld)
-	http.HandleFunc("/versionz", HandlerVersionz)
-	logrus.Fatal(http.ListenAndServe(":8080", nil))
+
+	http.HandleFunc("/helloworld", utilities.HandlerHelloworld)
+	http.HandleFunc("/versionz", utilities.HandlerVersionz)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- http-service/pkg/utilities/utilities.go
+++ http-service/pkg/utilities/utilities.go
@@ -1,6 +1,106 @@
 package utilities
 
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/fatih/camelcase"
+	log "github.com/sirupsen/logrus"
+)
+
+// compile time variables
+var (
+	GitCommit   string
+	ProjectName string
+)
+
+const (
+	isoFormat = "2006-01-02"
+)
+
+// struct for versionz response marshal
 type ResponseVersionz struct {
 	GitCommit   string `json:"git_commit"`
 	ProjectName string `json:"project_name"`
+}
+
+// struct for logging
+type Log struct {
+	Status    int    `json:"http_status"`
+	Timestamp string `json:"timestamp"`
+	Request   string `json:"request"`
+}
+
+// handler for the /helloworld endpoint
+func HandlerHelloworld(w http.ResponseWriter, r *http.Request) {
+
+	var status int
+	var message string
+
+	defer LogRequest(&status, r)
+
+	switch r.Method {
+	case "GET":
+		q := r.URL.Query()
+		if name, isIn := q["name"]; isIn {
+			status = http.StatusOK
+			message = "Hello " + strings.Join(camelcase.Split(name[0]), " ")
+		} else {
+			status = http.StatusOK
+			message = "Hello Strainger"
+		}
+	default:
+		status = http.StatusNotFound
+		message = "Method not found!"
+	}
+
+	w = writeResponse(w, message, status)
+}
+
+// handler for the /versionz endpoint
+func HandlerVersionz(w http.ResponseWriter, r *http.Request) {
+
+	var status int
+	var message string
+
+	defer LogRequest(&status, r)
+
+	switch r.Method {
+	case "GET":
+		resStruct := &ResponseVersionz{GitCommit: GitCommit,
+			ProjectName: ProjectName}
+
+		resBytes, err := json.Marshal(resStruct)
+		if err != nil {
+
+			status = http.StatusInternalServerError
+			message = "Internal Server Error!"
+
+			break
+		}
+		status = http.StatusOK
+		message = string(resBytes)
+	default:
+		status = http.StatusNotFound
+		message = "Method not found!"
+	}
+
+	w = writeResponse(w, message, status)
+}
+
+func LogRequest(status *int, r *http.Request) {
+
+	log.WithFields(log.Fields{
+		"status": *status,
+		"query":  r.URL.RawQuery,
+	}).Info(r.Method + " " + r.URL.Host + r.URL.Path)
+}
+
+func writeResponse(w http.ResponseWriter, message string, status int) http.ResponseWriter {
+
+	w.WriteHeader(status)
+	w.Write([]byte(message))
+
+	return w
 }
