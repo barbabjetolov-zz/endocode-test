fa929e2cf2af788bf5043f105f31035a73f7bf3a
barbabjetolov <rizz.edoardo@gmail.com>
2021-03-22 23:12:27 +0100

    update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,16 @@
-
 provider "helm" {
   kubernetes {
-    host                   = "https://192.16849.2:8443"
+    host                   = "https://192.168.49.2:8443"
 
-    client_certificate     = file("/client.crt")
-    client_key             = file("/client.key")
-    cluster_ca_certificate = file("/ca.crt")
+    client_certificate     = file("client.crt")
+    client_key             = file("client.key")
+    cluster_ca_certificate = file("ca.crt")
   }
 }
 
-resource "helm_release" "complete" {
-    name    = "http-request"
-    chart   = "/http-request-chart"
+resource "helm_release" "http-service" {
+    name    = "http-service"
+    chart   = "./http-service-chart"
 
     set {
         name    = "service.type"
