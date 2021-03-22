5472ee88dce6b1a93b8f557e44f8f659b0628d8e
barbabjetolov <rizz.edoardo@gmail.com>
2021-03-22 22:38:26 +0100

    add terraform plan

---
+++ http-service-chart/main.tf
@@ -0,0 +1,15 @@
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://192.16849.2:8443"
+
+    client_certificate     = file("/client.crt")
+    client_key             = file("/client.key")
+    cluster_ca_certificate = file("/ca.crt")
+  }
+}
+
+resource "helm_release" "complete" {
+    name    = "http-request"
+    chart   = "/http-request-chart"
+}
