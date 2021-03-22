8d6873cb13a81ce7e11f8e47f84c3cf1c7393895
barbabjetolov <rizz.edoardo@gmail.com>
2021-03-22 23:31:15 +0100

    helm install -> upgrade

--- main.tf
+++ main.tf
@@ -11,9 +11,11 @@ provider "helm" {
 resource "helm_release" "http-service" {
     name    = "http-service"
     chart   = "./http-service-chart"
-
+    force_update = true
     set {
         name    = "service.type"
         value   = "ClusterIP"
     }
+
+    reuse_values = true
 }
