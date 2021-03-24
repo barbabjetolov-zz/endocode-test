96c75cc2119f16ca571e10454ac466b39d436a87
barbabjetolov <rizz.edoardo@gmail.com>
2021-03-24 23:12:12 +0100

    changed namespace from logging to monitoring

--- main_ekf.tf
+++ main_ekf.tf
@@ -3,7 +3,7 @@ resource "helm_release" "elasticsearch" {
 	repository = "https://helm.elastic.co"
 	chart = "elasticsearch"
 
-	namespace = "logging"
+	namespace = "monitoring"
 
 	set {
 		name = "replicas"
@@ -21,19 +21,27 @@ resource "helm_release" "kibana" {
 	repository = "https://helm.elastic.co"
 	chart = "kibana"
 
-	namespace = "logging"
+	namespace = "monitoring"
+}
+
+resource "helm_release" "metricbeat" {
+	name = "metricbeat"
+	repository = "https://helm.elastic.co"
+	chart = "metricbeat"
+
+	namespace = "monitoring"	
 }
 
 resource "helm_release" "fluentd" {
     name = "fluentd"
     chart = "./fluentd-chart"
 
-	namespace = "logging"
+	namespace = "monitoring"
 }
 
 resource "helm_release" "kibana-ingress" {
     name = "kibana-ingress"
     chart = "./kibana-ingress-chart"
 
-        namespace = "logging"
+        namespace = "monitoring"
 }
